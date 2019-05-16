503dddba305f3ab8d4affe1017aae6a328a95e30
phongle95 <phongvippro1995@gmail.com>
2019-05-16 09:01:30 +0700

    phong cty update

--- public/ckfinder/config.php
+++ public/ckfinder/config.php
@@ -66,9 +66,9 @@ $config['images'] = array(
 $config['backends'][] = array(
     'name'         => 'default',
     'adapter'      => 'local',
-    'baseUrl'      => 'http://127.0.0.1:8000/public/upload',
+    'baseUrl'      => 'http://mayxonghoi24h.com/public/upload',
     // http://127.0.0.1:8000/public/upload
-    // http://lysonvn.com/public/upload
+    // http://mayxonghoi24h.com/public/upload
 //  'root'         => '', // Can be used to explicitly set the CKFinder user files directory.
     'chmodFiles'   => 0777,
     'chmodFolders' => 0755,
